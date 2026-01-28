d7f66b4d669beff1cd0e0e2f539303e4fc80cb5b
RacSche <rachel.scheck@posteo.de>
2026-01-28 12:09:29 -0500

    Update Q4ddPCR_Workflow.R

--- Q4ddPCR_Workflow.R
+++ Q4ddPCR_Workflow.R
@@ -1,109 +1,117 @@
-# ======= 0. define variables ==========
 library(MultiplexPCRAnalyser)
-getwd()
-source("/path/set_parameters.R") # put the path to the set_parameters.R file here
 
+# Load parameters
+source("/Users/rachelscheck/Documents/Jones Lab/Q4ddPCR/Manuscript/Code/Vignette Release/set_parameters.R")
+information <- read_files(
+  xlsx_file,
+  csv_file,
+  csv_skip,
+  remove_channel,
+  rm_zero_channel_wells
+)
 
-# ======= 1. read files ==========
-# that should be mostly untouched I think (for now, keep as before)
-# read files
-information <- read_files(xlsx_file, csv_file, csv_skip, remove_channel, rm_zero_channel_wells)
+# Read input files
 in_csv <- information[[1]]
-dtQC <- information[[2]]
+dtQC   <- information[[2]]
 
-# do some quality controls like sufficient droplets here already!
-# add stuff like dilution factor here already
-
-# QC checks
-## check for sufficient droplets
+# Quality control and preprocessing
 dtQC <- sufficient_droplets(dtQC, threshold)
 dtQC <- add_dilution_factor(dtQC, dilution_factor)
-
-# revise this, but keep at this position
-if(!all(unique(dtQC$`Sample description 1`) %in% names(tar_mio_factor))){
-  warning("Set tar_mio to 1")
-  # TODO: do
+if (!all(unique(dtQC$`Sample description 1`) %in% names(tar_mio_factor))) {
+  warning("Set tar_mio_factor to 1")
+  s_desc <- unique(dtQC$`Sample description 1`)
+  tar_mio_factor <- setNames(rep(1, length(s_desc)), s_desc)
 }
-# ======= 2. split tables ==========
-# if possible, split H2O, RPP, and experiment data into different tables
-# this should be based on dtQC$Target
-shear_wells <- unique(dtQC[dtQC$Target %in% shear_name,"Well"])
-water_wells <- unique(dtQC[dtQC$Target %in% water_name ,"Well"])
-data_wells <- setdiff(unique(dtQC$Well), union(shear_wells, water_wells))
 
-# add group ids to well based on `Sample description 1` and combination of targets
+# Split wells and tables
+shear_wells <- unique(dtQC[dtQC$Target %in% shear_name, "Well"])
+water_wells <- unique(dtQC[dtQC$Target %in% water_name, "Well"])
+data_wells  <- setdiff(unique(dtQC$Well), union(shear_wells, water_wells))
+
 group_ids <- get_group_id(dtQC)
 dtQC$group_id <- group_ids[dtQC$Well]
+shear_table <- dtQC[dtQC$Well %in% shear_wells, ]
+water_table <- dtQC[dtQC$Well %in% water_wells, ]
+water_csv   <- in_csv[in_csv$Well %in% water_wells, ]
 
-# split tables
-shear_table <- dtQC[dtQC$Well %in% shear_wells,]
-water_table <- dtQC[dtQC$Well %in% water_wells,]
-water_csv <- in_csv[in_csv$Well %in% water_wells,]
-data_table <- dtQC[dtQC$Well %in% data_wells,]
-data_csv <- in_csv[in_csv$Well %in% data_wells,]
+data_table  <- dtQC[dtQC$Well %in% data_wells, ]
+data_csv    <- in_csv[in_csv$Well %in% data_wells, ]
 
-# remove Ch columns from data table
-data_table <- data_table[,grep("Ch", colnames(data_table), value=T, invert = T)]
-# ======= 3. do shearing computations ==========
-# should translate quickly
-shear_table <- compute_shearing_factor(shear_table, mean_copies_factor,
-                                       mean_cells_per_reac_factor)
+data_table <- data_table[
+  , grep("Ch", colnames(data_table), value = TRUE, invert = TRUE)
+]
 
-# ======= 4. do table computations ==========
-# this changes a looooooot
-# much of the for loop can be done without splitting the data and then later by 
-# grouping along the added group_ID column
+# Shearing factor computation
+shear_table <- compute_shearing_factor(
+  shear_table,
+  mean_copies_factor,
+  mean_cells_per_reac_factor
+)
 
-
-# create one (!) confusion matrix
-conf_mat <- create_confusion_matrix(data_csv, data_table, ch_dye, target_channel) # add arguments
-
-# combine tables
+# Main table computations
+conf_mat <- create_confusion_matrix(
+  data_csv,
+  data_table,
+  ch_dye,
+  target_channel
+)
 tab <- merge_tables(data_table, conf_mat, shear_table)
-
-# compute means per target
 tab <- compute_target_means(tab)
 
-
-# get multiple positives
-## keep function but adapt grouping to account for group_id
-
-combinations <- get_multipos(c("Psi", "Env", "Pol", "Gag")) # can be a parameter or extracted from data somwhow
-
-for (multip in combinations) {
+# Multi-positive analysis
+for (multip in multi_positives) {
   tab <- get_multi_pos(tab, multip, tar_mio_factor)
 }
 
-# compute total HIV
-total_HIV_dict <- setNames(unlist(lapply(unique(tab$group_id), function(x){
-  compute_total_HIV(tab[tab$group_id == x,])
-})), unique(tab$group_id))
-tab[["total HIV DNA/Mio cells"]] <- total_HIV_dict[as.character(tab$group_id)]
+# Total HIV quantification
+total_HIV_dict <- setNames(
+  unlist(lapply(unique(tab$group_id), function(x) {
+    compute_total_HIV(tab[tab$group_id == x, ])
+  })),
+  unique(tab$group_id)
+)
 
+tab[["total HIV DNA/Mio cells"]] <-
+  total_HIV_dict[as.character(tab$group_id)]
 
-# compute envPsi total HIV
-total_HIV_dict_envpsi <- setNames(unlist(lapply(unique(tab$group_id), function(x){
-  compute_total_HIV_envPsi(tab[tab$group_id == x,])
-})), unique(tab$group_id))
+total_HIV_dict_envpsi <- setNames(
+  unlist(lapply(unique(tab$group_id), function(x) {
+    compute_total_HIV_envPsi(tab[tab$group_id == x, ])
+  })),
+  unique(tab$group_id)
+)
 
-tab[["total HIV DNA/Mio cells (Env.Psi)"]] <- total_HIV_dict_envpsi[as.character(tab$group_id)]
+tab[["total HIV DNA/Mio cells (Env.Psi)"]] <-
+  total_HIV_dict_envpsi[as.character(tab$group_id)]
 
-
-### continue here
-####################
-
-# also get information for H2O tables if necessary
-if(length(water_wells) > 0){
-  # create one (!) confusion matrix for the water table
-  h2o_conf_mat <- create_confusion_matrix(water_csv, water_table, ch_dye, target_channel) # add arguments
+# Water control analysis
+if (length(water_wells) > 0) {
+  h2o_conf_mat <- create_confusion_matrix(
+    water_csv,
+    water_table,
+    ch_dye,
+    target_channel
+  )
   
-  # combine water tables
   h2o_tab <- merge_tables(water_table, h2o_conf_mat, shear_table)
-  h2o_tab <- h2o_tab[,1:(ncol(h2o_tab)-2)]
+  h2o_tab <- h2o_tab[, 1:(ncol(h2o_tab) - 2)]
 } else {
   h2o_tab <- NULL
 }
-# ======= 5. output to xlsx file ==========
-# this might need some adaptations
-out_tables <- lapply(unique(tab$group_id), function(x){tab[tab$group_id == x,grep("group_id", names(tab), value=T, invert=T)]})
-write_output_file(out_tables, conf_mat, output_file, h2o_tab, multi_positives, shear_table)
+
+# Output generation
+out_tables <- lapply(unique(tab$group_id), function(x) {
+  tab[
+    tab$group_id == x,
+    grep("group_id", names(tab), value = TRUE, invert = TRUE)
+  ]
+})
+
+write_output_file(
+  out_tables,
+  conf_mat,
+  output_file,
+  h2o_tab,
+  multi_positives,
+  shear_table
+)
